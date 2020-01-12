76fae54495ddf291815091bdd7a8d4e50dec222a
Shahul Hameed <beingshahul@gmail.com>
2020-01-12 23:24:37 +0100

    Add a new test for verifying the proper template.

--- pages/tests.py
+++ pages/tests.py
@@ -6,8 +6,8 @@ from django.urls import reverse
 class HomepageTests(SimpleTestCase):
     '''
     Tests to ensure the homepage returns
-    expected status code and the URL name is 
-    correct. 
+    expected status code and the URL name is
+    correct.
     '''
 
     def test_homepage_status_code(self):
@@ -17,3 +17,7 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_url_name(self):
         response = self.client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
+
+    def test_homepage_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
