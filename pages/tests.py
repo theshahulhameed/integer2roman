4368272639486becae916fea23c85fabb726a218
Shahul Hameed <beingshahul@gmail.com>
2020-01-12 23:13:32 +0100

    Write test for Homepage.

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+#from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTests(SimpleTestCase):
+    '''
+    Tests to ensure the homepage returns
+    expected status code and the URL name is 
+    correct. 
+    '''
+
+    def test_homepage_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url_name(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
