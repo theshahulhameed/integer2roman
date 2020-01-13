80207ca3e2d6395ab30818427d2694d9a571b9ba
Shahul Hameed <beingshahul@gmail.com>
2020-01-13 01:06:01 +0100

    Create view to handle the input and render it to a template.

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,24 @@
 from django.views.generic import TemplateView
+from .utils import integerToRoman
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        data = super().get_context_data(**kwargs)
+        if self.request.GET.get('integer'):
+            try:
+                integer = int(self.request.GET.get('integer'))
+            except ValueError as error:
+                data['error'] = 'Enter an integer input, try again.'
+            else:
+                try:
+                    data['romannumeral'] = integerToRoman(integer)
+                    data['integer'] = integer
+                except ValueError as error:
+                    data['error'] = error
+        return data
+
+
+
