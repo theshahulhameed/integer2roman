e3a250b6769555a0176a4cc44ca2e73e947a0dec
Shahul Hameed <beingshahul@gmail.com>
2020-01-13 01:05:03 +0100

    Add more tests to take care of the util function to handle conversion of integer to roman.

--- pages/tests.py
+++ pages/tests.py
@@ -1,11 +1,11 @@
-#from django.test import TestCase
 from django.test import SimpleTestCase
 from django.urls import reverse
+from pages.utils import integerToRoman
 
 
 class HomepageTests(SimpleTestCase):
     '''
-    Tests to ensure the homepage returns
+    Django specific tests to ensure the homepage returns
     expected status code and the URL name is
     correct.
     '''
@@ -21,3 +21,45 @@ class HomepageTests(SimpleTestCase):
     def test_homepage_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+
+class IntegerToRomanTests(SimpleTestCase):
+    '''
+    Test to ensure the utility function we created
+    gives expected results for the inputs
+    '''
+
+    def test_with_input_as_four(self):
+        input = 4
+        result = integerToRoman(input)
+        self.assertNotEqual(result, 'IIII')
+
+    def test_input_less_than_five(self):
+        input = 4
+        result = integerToRoman(input)
+        self.assertEqual(result, 'IV')
+
+    def test_with_input_as_three(self):
+        input = 3
+        result = integerToRoman(input)
+        self.assertEqual(result, 'III')
+
+    def test_with_input_as_nine(self):
+        input = 9
+        result = integerToRoman(input)
+        self.assertEqual(result, 'IX')
+
+    def test_with_input_as_thirty_seven(self):
+        input = 37
+        result = integerToRoman(input)
+        self.assertEqual(result, 'XXXVII')
+
+    def test_with_input_as_ninteeen_ninety_three(self):
+        input = 1993
+        result = integerToRoman(input)
+        self.assertEqual(result, 'MCMXCIII')
+
+    def test_bad_type(self):
+        data = "apple"
+        with self.assertRaises(TypeError):
+            result = integerToRoman(data)
