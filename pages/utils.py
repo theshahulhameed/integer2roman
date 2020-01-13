d0181584169f97b3108a3cac88b829465eb0ce75
Shahul Hameed <beingshahul@gmail.com>
2020-01-13 01:55:58 +0100

    Make necessary changes to util function to handle the edge cases.

--- pages/utils.py
+++ pages/utils.py
@@ -1,8 +1,9 @@
 def integerToRoman(integer):
-    if integer == 0 or integer > 4000:
-        raise ValueError("This number can't represtented by a Roman numeral. Try a number within the range 1-3999")
+    if integer <= 0 or integer > 4000:
+        raise ValueError("This number can't represtented by a Roman numeral.Try a number within the range 1 - 3999")
     else:
-        list_of_symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        list_of_symbols = ["M", "CM", "D", "CD", "C",
+                           "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
         numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
         result = ""
         for letter, n in zip(list_of_symbols, numbers):
