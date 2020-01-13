778fc10c4cf7cb46b4ad43ff78e995e84198a98f
Shahul Hameed <beingshahul@gmail.com>
2020-01-13 01:05:35 +0100

    Create Integer2Roman util function to take care of the conversion and handle the various exceptions.

---
+++ pages/utils.py
@@ -0,0 +1,11 @@
+def integerToRoman(integer):
+    if integer == 0 or integer > 4000:
+        raise ValueError("This number can't represtented by a Roman numeral.")
+    else:
+        list_of_symbols = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"]
+        numbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        result = ""
+        for letter, n in zip(list_of_symbols, numbers):
+            result += letter * int(integer / n)
+            integer %= n
+        return result
